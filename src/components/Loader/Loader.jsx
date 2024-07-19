d44ed4ec980e4c8c1c3a78508221c2e09bad3b40
Inkom007 <inkom0703@gmail.com>
2024-07-19 23:26:16 +0100

    change loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,13 @@
-import { RotatingLines } from "react-loader-spinner";
+import { InfinitySpin } from "react-loader-spinner";
 import s from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={s.loader}>
-      <RotatingLines
+      <InfinitySpin
         visible={true}
-        height="96"
-        width="96"
-        color="red"
-        backgroundColor="red"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
+        width="200"
+        color="rgb(196, 90, 24)"
+        ariaLabel="infinity-spin-loading"
       />
     </div>
   );
